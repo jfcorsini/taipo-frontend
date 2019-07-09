6b487236cb2a9220ff0c0352e971874c65e9c69a
João Corsini <joao.corsini@f-secure.com>
2019-07-09 20:46:57 +0300

    feat: Add Amplify Authenticator
    Following steps based on Nader Dabit's post (https://dev.to/dabit3/how-to-build-production-ready-vue-authentication-23mk) I built the basic structure of the website.

---
+++ src/components/Auth.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="auth">
+    <amplify-authenticator></amplify-authenticator>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "auth"
+};
+</script>
+
+<style>
+.auth {
+  margin: 0 auto;
+  width: 460px;
+}
+</style>
---
+++ src/components/Chat.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>This is where the chat will be placed!</h1>
+</template>
+
+<script>
+import { Auth } from "aws-amplify";
+
+export default {
+  name: "protected"
+};
+</script>
