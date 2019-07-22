fc1945ab2e6166207f4abd1555633f75e0d954d4
João Corsini <jf.corsini@gmail.com>
2019-07-22 18:32:47 +0300

    feat: List all messages of a group

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="home" v-if="hydrated">
-    <amplify-connect :query="listChatsQuery">
+    <amplify-connect :query="listChatMessagesQuery">
       <template slot-scope="{loading, data, errors}">
         <div v-if="loading">Loading...</div>
 
         <div v-else-if="errors.length > 0"></div>
         <div v-else-if="data">
-          <div v-for="item in data.listChats.items" v-bind:key="item.id">
-            <h1>{{ item }}</h1>
-            <h1>{{ item.id }}</h1>
-            <p>{{ item.name }}</p>
+          <div v-for="item in data.listMessages" v-bind:key="item.messageId">
+            <p>Message: {{ item.message }}</p>
           </div>
         </div>
       </template>
@@ -20,14 +18,15 @@
 <script>
 import { components } from "aws-amplify-vue";
 
-const listChatsQuery = `query listChats {
-    listChats {
-      items {
-        id
-        name
-      }
-    }
-  }`;
+const chatId = "chatOne"; // Hardcoded until you can choose the chat name
+const listChatMessagesQuery = `query listMessages {
+  listMessages(input: {chatId: "${chatId}"}) {
+    chatId
+    message
+    sortKey
+    messageId
+  }
+}`;
 
 export default {
   name: "chat",
@@ -48,8 +47,8 @@ export default {
   },
 
   computed: {
-    listChatsQuery() {
-      return this.$Amplify.graphqlOperation(listChatsQuery);
+    listChatMessagesQuery() {
+      return this.$Amplify.graphqlOperation(listChatMessagesQuery);
     }
   }
 };
