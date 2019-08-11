e9bc6c5be1ad1196193f7295a4a4456707883c21
João Corsini <jf.corsini@gmail.com>
2019-08-11 21:37:14 +0300

    feat: Create chat from User profile
    In case there is already a chat (private) between the users, then open the chat page.

--- src/components/User.vue
+++ src/components/User.vue
@@ -10,6 +10,28 @@
           <h3>Email: {{ data.getUser.email }}</h3>
           <p>Last login: {{ data.getUser.lastLogin }}</p>
         </div>
+
+        <amplify-connect :query="getChatPrivate">
+          <template slot-scope="{loading, data, errors}">
+            <div v-if="loading">Checking for chat...</div>
+
+            <div v-else-if="errors.length > 0"></div>
+            <div v-else-if="data">
+              <div v-if="hasPrivateChat(data)">
+                <router-link v-bind:to="'/chats/' + data.getChatPrivate.chatId">
+                  <button>Go to chat</button>
+                </router-link>
+              </div>
+              <div v-else>
+                <amplify-connect :mutation="createChat" @done="onCreateChatFinished">
+                  <template slot-scope="{ loading, mutate }">
+                    <button :disabled="loading" @click="mutate">Start chatting</button>
+                  </template>
+                </amplify-connect>
+              </div>
+            </div>
+          </template>
+        </amplify-connect>
       </template>
     </amplify-connect>
   </div>
@@ -19,13 +41,25 @@
 import { Auth } from "aws-amplify";
 import { components } from "aws-amplify-vue";
 
-const getUser = `query getUser($username: String!) {
+const getUserQuery = `query getUser($username: String!) {
   getUser(input: {username: $username}) {
     username
     email
     lastLogin
   }
 }`;
+
+const getChatPrivateQuery = `query getChatPrivate($username: String!) {
+  getChatPrivate(input: {username: $username}) {
+    chatId
+  }
+}`;
+
+const createChatPrivateMutation = `mutation createChat($username: String!) {
+    createChat(input: { username: $username }) {
+      chatId
+    }
+  }`;
 
 export default {
   name: "chat",
@@ -47,9 +81,30 @@ export default {
 
   computed: {
     getUser() {
-      return this.$Amplify.graphqlOperation(getUser, {
+      return this.$Amplify.graphqlOperation(getUserQuery, {
         username: this.$route.params.username
       });
+    },
+    getChatPrivate() {
+      return this.$Amplify.graphqlOperation(getChatPrivateQuery, {
+        username: this.$route.params.username
+      });
+    },
+    createChat() {
+      return this.$Amplify.graphqlOperation(createChatPrivateMutation, {
+        username: this.$route.params.username
+      });
+    }
+  },
+
+  methods: {
+    onCreateChatFinished(mutationResponse) {
+      const chatId = mutationResponse.data.createChat.chatId;
+      this.$router.push(`/chats/${chatId}`);
+    },
+    hasPrivateChat(result) {
+      console.log("RESULT FROM QUERY", result);
+      return result.getChatPrivate && result.getChatPrivate.chatId;
     }
   }
 };
