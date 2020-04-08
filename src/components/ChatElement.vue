7f1b5c29a449bb3d1a3392e208a669ec7b701fd4
João Corsini <jf.corsini@gmail.com>
2020-04-08 11:23:58 +0300

    feat: Show user image in chat list
    It's now easier to identity if the chat is with user or a chat gorup. Next step we add image to the chat group.

--- src/components/ChatElement.vue
+++ src/components/ChatElement.vue
@@ -2,8 +2,9 @@
   <div class="h-full">
     <div v-if="!loading" class="bg-white shadow-xs mb-2 mx-px font-light text-base cursor-pointer hover:bg-green-300 active:bg-green-400">
       <router-link tag="p" v-bind:to="'/chats/' + chatId" class="inline-block h-16 w-full" v-bind:activeClass="'bg-green-400 shadow-lg'">
-      <div class="ml-8 mt-6">
-        {{ chatName }}
+      <div class="ml-4 mt-2" v-bind:class="{ 'ml-8 mt-6': !userIdentityId }">
+        <user-image v-if="userIdentityId" class="h-12 w-12 rounded-full inline" v-bind:identityId="userIdentityId"/>
+        <span>{{ chatName }}</span>
       </div>
       </router-link>
     </div>
@@ -12,6 +13,7 @@
 
 <script>
 import { API, Auth, graphqlOperation } from "aws-amplify";
+import UserImage from './UserImage';
 
 const getChatQuery = `query getChat($chatId: String!) {
   getChat(input: { chatId: $chatId }) {
@@ -29,6 +31,12 @@ const getChatQuery = `query getChat($chatId: String!) {
   }
 }`;
 
+const getUserQuery = `query getUser($username: String!) {
+  getUser(input: {username: $username}) {
+    identityId
+  }
+}`;
+
 export default {
   name: "chat-element",
 
@@ -37,8 +45,13 @@ export default {
   data() {
     return {
       loading: true,
+      userIdentityId: null,
       chatName: ""
     };
+  },
+
+  components: {
+    UserImage,
   },
 
   async mounted() {
@@ -50,18 +63,24 @@ export default {
     );
     const chat = response.data.getChat || {};
 
-    if (chat.private) {
-      const usernames = chat.usernames;
-      let chatWith = usernames[0];
-      if (chatWith === user.username) {
-        chatWith = usernames[1];
-      }
-
-      this.chatName = `Private chat with ${chatWith}`;
-    } else {
+    if (!chat.private) {
       this.chatName = chat.chatName;
+      this.loading = false;
+      return;
     }
+
+    const usernames = chat.usernames;
+    let chatWith = usernames[0];
+    if (chatWith === user.username) {
+      chatWith = usernames[1];
+    }
+    this.chatName = chatWith;
     this.loading = false;
+
+    const userResponse = await API.graphql(
+      graphqlOperation(getUserQuery, { username: chatWith })
+    );
+    this.userIdentityId = userResponse.data.getUser.identityId;
   }
 };
 </script>
