3a0737c688a455c45da4f41a6925a898488f8121
João Corsini <jf.corsini@gmail.com>
2020-04-14 20:54:04 +0300

    feat: Start trying twilio-video
    Not working yet, but one step closer!

--- src/components/Pages/Home.vue
+++ src/components/Pages/Home.vue
@@ -1,8 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <div id="local-media"></div>
+    <div id="remote-media-div"></div>
+  </div>
 </template>
 
 <script>
+/* eslint-disable no-console */
+
+import { connect, createLocalVideoTrack } from 'twilio-video';
+createLocalVideoTrack().then(track => {
+  const localMediaContainer = document.getElementById('local-media');
+  localMediaContainer.appendChild(track.attach());
+});
+
+const token = '';
+
+connect(token, { name:'b24ad927-4c17-4a7c-9f12-1096456552a0' }).then(room => {
+  console.log(`Successfully joined a Room: ${room}`);
+  room.on('participantConnected', participant => {
+    console.log(`A remote Participant connected: ${participant}`);
+  });
+}, error => {
+  console.error(`Unable to connect to Room: ${error.message}`);
+});
 
 export default {
   name: "home",
