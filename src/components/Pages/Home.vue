250d46a4be8bb0f7184369d227319d7b76a1bc2b
João Corsini <jf.corsini@gmail.com>
2020-04-16 18:22:31 +0300

    feat: Test video the home page

--- src/components/Pages/Home.vue
+++ src/components/Pages/Home.vue
@@ -8,24 +8,76 @@
 <script>
 /* eslint-disable no-console */
 
+import { API, graphqlOperation } from "aws-amplify";
 import { connect, createLocalVideoTrack } from 'twilio-video';
+
 createLocalVideoTrack().then(track => {
   const localMediaContainer = document.getElementById('local-media');
   localMediaContainer.appendChild(track.attach());
 });
 
-const token = '';
+const getTwilioToken = `query getTwilioToken($roomName: String!) {
+  getTwilioToken(input: { roomName: $roomName }) {
+    username
+    token    
+  }
+}`;
 
-connect(token, { name:'b24ad927-4c17-4a7c-9f12-1096456552a0' }).then(room => {
-  console.log(`Successfully joined a Room: ${room}`);
-  room.on('participantConnected', participant => {
-    console.log(`A remote Participant connected: ${participant}`);
-  });
-}, error => {
-  console.error(`Unable to connect to Room: ${error.message}`);
-});
 
 export default {
   name: "home",
+
+  async mounted() {
+    await this.$apollo.provider.defaultClient.hydrated();
+
+    const response = await API.graphql(
+      graphqlOperation(getTwilioToken, { roomName: 'home' })
+    );
+
+    const { token, username } = response.data.getTwilioToken;
+    console.log('Token', token);
+    console.log('Username', username);
+
+    connect(token, { name:'home', audio: true, video: { width: 640 } }).then(room => {
+      console.log(`Successfully joined a Room: ${room}`);
+
+      const localParticipant = room.localParticipant;
+      console.log(`Connected to the Room as LocalParticipant "${localParticipant.identity}"`);
+
+      // Log any Participants already connected to the Room
+      room.participants.forEach(participant => {
+        console.log(`Participant "${participant.identity}" is connected to the Room`);
+      });
+
+      room.once('participantConnected', participant => {
+        console.log(`Participant "${participant.identity}" has connected to the Room`);
+      });
+
+      room.once('participantDisconnected', participant => {
+        console.log(`Participant "${participant.identity}" has disconnected from the Room`);
+      })
+
+      room.on('participantConnected', participant => {
+        console.log(`Participant "${participant.identity}" connected`);
+
+        participant.tracks.forEach(publication => {
+          if (publication.isSubscribed) {
+            const track = publication.track;
+            document.getElementById('remote-media-div').appendChild(track.attach());
+          }
+        });
+
+        participant.on('trackSubscribed', track => {
+          document.getElementById('remote-media-div').appendChild(track.attach());
+        });
+      });
+
+      room.on('participantDisconnected', participant => {
+        console.log(`Participant disconnected: ${participant.identity}`);
+      });
+    }, error => {
+      console.error(`Unable to connect to Room: ${error.message}`);
+    });
+  }
 };
 </script>
