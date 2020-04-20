b6c98ded3a15e6a3a944800caca6e2beb61bd447
João Corsini <jf.corsini@gmail.com>
2020-04-20 23:10:41 +0300

    refactor: Remove unnused SCSS files

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,6 @@ module.exports = {
       sass: {
         data: `
           @import "@/assets/scss/_reset.scss";
-          @import "@/assets/scss/_variables.scss";
-          @import "@/assets/scss/_mixins.scss";
         `
       }
     }
